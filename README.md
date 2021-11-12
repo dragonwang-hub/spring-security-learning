40f29d4a2846748fe11290a23e4da9b64662d86c
yunlong.wang <ylwang9627@foxmail.com>
2021-11-12 22:46:52 +0800

    yl #000 chapter3 init controller

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #Spring Security Learning
 1. Securing a web application: https://spring.io/guides/gs/securing-web/
 2. Security 包 自带验证页面，只有单用户，密码在命令行可查。
+3. 默认数据库模型的认证与授权
---
+++ src/main/java/com/security/learning/chapter3/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.security.learning.chapter3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app/api")
+public class AppController {
+
+    @GetMapping("hello")
+    private String hello() {
+        return "Hello, App!";
+    }
+}
