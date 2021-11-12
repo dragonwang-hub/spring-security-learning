4b86a8ea1fa2c27af9195f738a27b781b113568d
yunlong.wang <ylwang9627@foxmail.com>
2021-11-12 23:03:16 +0800

    yl #000 use UserDetailsService create inMemory user instant

---
+++ src/main/java/com/security/learning/chapter3/configuration/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.security.learning.chapter3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/api/**").hasRole("ADMIN")
+                .antMatchers("/user/api/**").hasRole("USER")
+                .antMatchers("/app/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        inMemoryUserDetailsManager.createUser(User.withUsername("admin").password("password").roles("ADMIN").build());
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
