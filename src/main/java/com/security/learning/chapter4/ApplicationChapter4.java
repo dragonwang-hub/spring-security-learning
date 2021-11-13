f41c6c4f5cb187260690496de4f78d0ec3b84864
yunlong.wang <ylwang9627@foxmail.com>
2021-11-13 20:00:43 +0800

    yl #000 user custom data modal, init

---
+++ src/main/java/com/security/learning/chapter4/ApplicationChapter4.java
@@ -0,0 +1,11 @@
+package com.security.learning.chapter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationChapter4 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationChapter4.class, args);
+    }
+}
