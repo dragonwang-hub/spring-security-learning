af4850ba5573c3ded701c9952b3d992686c5633b
yunlong.wang <ylwang9627@foxmail.com>
2021-11-20 08:27:27 +0800

    yl #000 @Secured example

--- src/main/java/com/security/learning/chapter5/ApplicationChapter5.java
+++ src/main/java/com/security/learning/chapter5/ApplicationChapter5.java
@@ -2,8 +2,10 @@ package com.security.learning.chapter5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class ApplicationChapter5 {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationChapter5.class, args);
--- src/main/java/com/security/learning/chapter5/controller/HelloController.java
+++ src/main/java/com/security/learning/chapter5/controller/HelloController.java
@@ -1,18 +1,24 @@
 package com.security.learning.chapter5.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Secured("ROLE_USER")
     @GetMapping("/user/api/hello")
     public String helloUser() {
+        System.out.println("enter hello user");
         return "hello user";
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("admin/api/hello")
     public String helloAdmin() {
+        System.out.println("enter hello admin");
         return "hello admin";
     }
 }
