a8e8cd4d3b305ffd483934d25917041a903124f1
yunlong.wang <ylwang9627@foxmail.com>
2021-11-20 08:24:58 +0800

    yl #000 fix chapter4

--- src/main/java/com/security/learning/chapter4/entity/CustomUserEntity.java
+++ src/main/java/com/security/learning/chapter4/entity/CustomUserEntity.java
@@ -10,8 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Entity
